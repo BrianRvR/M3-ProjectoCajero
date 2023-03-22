68c48501f82cbf25ead3362dd35d150dca7ab13d
BrianRvR <98058479+BrianRvR@users.noreply.github.com>
2023-03-22 08:31:48 +0100

    Add files via upload

---
+++ cajeroautomatico/src/main/java/com/mycompany/cajeroautomatico/App.java
@@ -0,0 +1,44 @@
+package com.mycompany.cajeroautomatico;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+    private static Scene scene;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        scene = new Scene(loadFXML("primary"), 640, 480);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+
+    public static void main(String[] args) {
+        Cliente brian = new Cliente();
+        brian.setUsuario("Brian");
+        Cliente joaco = new Cliente("Joaco", "12345", false);
+        
+        System.out.println("Cliente brian: "+brian.getUsuario());
+        System.out.println("Cliente joaco: "+joaco.getUsuario());
+        launch();
+    }
+
+}
