a20a283e611eabde943aa2b66909312fe2e05ca8
BrianRvR <98058479+BrianRvR@users.noreply.github.com>
2023-03-22 10:36:02 +0100

    Delete cajeroautomatico directory

--- cajeroautomatico/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.mycompany.cajeroautomatico {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.mycompany.cajeroautomatico to javafx.fxml;
-    exports com.mycompany.cajeroautomatico;
-}
